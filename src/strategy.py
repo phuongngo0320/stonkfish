736bfd3bfd1dec863c39130c5a266350a1086f82
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-03 23:40:41 +0700

    Add cache to speedup algo

--- src/strategy.py
+++ src/strategy.py
@@ -2,16 +2,26 @@ import math
 from src.game import Game
 from src.state import State
 
-def alpha_beta_cutoff_search(state: State, game: Game, d=4, cutoff_test=None, eval_fn=None):
+def cache1(function):
+    "Like lru_cache(None), but only considers the first argument of function."
+    cache = {}
+    def wrapped(x, *args):
+        if x not in cache:
+            cache[x] = function(x, *args)
+        return cache[x]
+    return wrapped
+
+def alpha_beta_cutoff_search(game: Game, state: State, d=4, cutoff_test=None, eval_fn=None):
     """Search game to determine best action; use alpha-beta pruning.
     This version cuts off search and uses an evaluation function."""
 
     player = game.to_move(state)
 
     # Functions used by alpha_beta
+    @cache1
     def max_value(state: State, alpha, beta, depth):
         if cutoff_test(state, depth):
-            return eval_fn(state)
+            return eval_fn(state, player)
         v = -math.inf
         for a in game.actions(state):
             v = max(v, min_value(game.result(state, a), alpha, beta, depth + 1))
@@ -20,9 +30,10 @@ def alpha_beta_cutoff_search(state: State, game: Game, d=4, cutoff_test=None, ev
             alpha = max(alpha, v)
         return v
 
+    @cache1
     def min_value(state: State, alpha, beta, depth):
         if cutoff_test(state, depth):
-            return eval_fn(state)
+            return eval_fn(state, player)
         v = math.inf
         for a in game.actions(state):
             v = min(v, max_value(game.result(state, a), alpha, beta, depth + 1))
