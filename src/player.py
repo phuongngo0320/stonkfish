229ebaeffd3536b8c7ed81e8abeafd1b963e4b9b
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-03 23:41:33 +0700

    Add players

--- src/player.py
+++ src/player.py
@@ -1,11 +1,24 @@
-from enum import Enum
 import random
+from src.evaluation import evaluate_material
 from src.game import Game
+from src.state import State
+from src.strategy import alpha_beta_cutoff_search
 
-def random_player(game: Game, state):
+def random_player(game: Game, state: State):
     actions = game.actions(state)
     if actions:
         return random.choice(game.actions(state))
     return None
 
-# TODO: AI players with different levels (1-10)
+def leveled_player(level=1):
+    
+    def player(game: Game, state: State):
+        return alpha_beta_cutoff_search(
+            game, 
+            state, 
+            d=(level-1)*2 + 1, 
+            cutoff_test=None, 
+            eval_fn=evaluate_material
+        )
+    
+    return player
