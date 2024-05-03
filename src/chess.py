dc785fa215dedae8c65618624eaefa72ff29fff6
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-03 20:58:38 +0700

    Implement chess methods

--- src/chess.py
+++ src/chess.py
@@ -1,5 +1,6 @@
 from src.move import Move
 from src.piece import PieceColor
+from src.result import ResultType
 from src.state import State
 from src.game import Game
 
@@ -13,12 +14,29 @@ class Chess(Game):
     def result(self, state: State, move: Move):
         return state.move(move.fromCell, move.toCell)
     
-    def utility(self, state: State, player):
-        pass
+    def utility(self, state: State, player: PieceColor):
+        
+        if state.result.type in [
+            ResultType.STALEMATE,
+            ResultType.INSUFFICIENT_MATERIAL,
+            ResultType.FIFTY_MOVES,
+            ResultType.SEVENTYFIVE_MOVES,
+            ResultType.THREEFOLD_REPETITION,
+            ResultType.FIVEFOLD_REPETITION
+        ]:
+            return 0
+        
+        if state.result.type == ResultType.CHECKMATE:
+            if state.result.winner == player:
+                return 1
+            else:
+                return -1
+            
+        raise Exception(f"Invalid leaf node state: {state}")
     
-    def terminal_test(self, state):
-        pass
+    def terminal_test(self, state: State):
+        return state.result.type != ResultType.NONE
     
-    def to_move(self, state):
-        pass
+    def to_move(self, state: State):
+        return state.to_move
         
