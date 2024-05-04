dd8282276acccb7f3c607fdff2d3c96ca3184a5f
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-04 22:38:50 +0700

    Modify main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from src.chess import Chess
-from src.fen import parseBoard
 from src.player import random_player
-from src.state import State
 
-# game = Chess()
-# game.play_game(random_player, random_player)
-
-board = State()
-print(board)
-
-board = board.move(parseMove)
+game = Chess()
+game.play_game(random_player, random_player)
